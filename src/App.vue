393646922ca9f536722dc998211e8a3e5e756cc9
Vitor Ferronato <vitorferronato@gmail.com>
2022-05-07 17:24:54 -0300

    Update app

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,43 @@
 <template>
   <div id="app">
+    <h1>Conversor de moedas</h1>
+    <div class='container'>
+      <ConversorApp  moeda-a='USD' moeda-b='BRL' />
+      <ConversorApp  moeda-a='BRL' moeda-b='USD' />
+      <ConversorApp  moeda-a='EUR' moeda-b='BRL' />
+      <ConversorApp  moeda-a='BRL' moeda-b='EUR' />
+    </div>
+
   </div>
 </template>
 
 <script>
+import ConversorApp from './components/ConversorApp.vue'
 
 export default {
   name: 'App',
   components: {
+    ConversorApp
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app{
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
+
+  .container{
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+    gap: 2rem;
+    align-items: center;
+    margin-top: 3rem;
+  }
+ 
 </style>
---
+++ src/components/ConversorApp.vue
@@ -0,0 +1,74 @@
+<template>
+<div class="conversor">
+    <h2>{{ moedaA }} to {{ moedaB }}</h2>
+    <input type="number" v-model="moedaA_value" :placeholder="moedaA">
+    <input class="button" type="button" value="Converter" @click="converter()">
+    <h2>{{moedaB_value}}</h2>
+</div>
+
+</template>
+
+<script>
+export default{
+    name: 'ConversorApp',
+    props: ['moedaA', 'moedaB'],
+
+    data() {
+        return{
+            moedaA_value: '',
+            moedaB_value: 0
+        }
+    },
+
+    methods: {
+        converter() {
+            //  SERVIDOR DA API FORA DO AR, CONSIDERANDO DOLLAR A R$ 5,08 ( Cotação  07/05/2022) -------
+
+            // let de_para = this.moedaA_value + '' + this.moedaB_value
+            // let url = `https://free.currconv.com/api/v7/convert?q=${de_para}&compact=ultra&apiKey=03944ee541bfa347c4a8`
+            // fetch(url).then(res=>{return res.json})
+            //             .then(json=> {
+            //             let cotacao = json[de_para].val
+            //             this.moedaA_value = (cotacao * parseFloat(this.moedaA_value)).toFixed(2)
+            // })
+
+            let dollarValue = 5.08
+            this.moedaB_value = (dollarValue * parseFloat(this.moedaA_value )).toFixed(2)
+        }
+    }
+}
+
+</script>
+
+<style scoped>
+
+    .conversor{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+
+        max-width: 400px;
+        min-width: 400px;
+        padding: 20px;
+        box-shadow: 0 4px 8px 0 rgba(0,0,0, .2);
+        border-radius: 5px;
+    }
+
+    .button{
+        margin-top: 1rem;
+        padding: 10px;
+        border-radius: 5px;
+        background-color: #2c3e50;
+        color: #fff;
+        cursor: pointer;
+        font-family: Arial, sans-serif;
+        border: none;
+        transition: .2s;
+    }
+    .button:hover{
+        opacity: .8;
+    }
+
+
+</style>
